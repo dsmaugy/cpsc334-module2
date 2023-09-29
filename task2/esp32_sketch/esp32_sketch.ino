5e393c615e099bcda992be0bfcfb6dffd32ee6df
Darwin Do <darwin78913@gmail.com>
2023-09-29 01:40:06 -0400

    bruh

--- task2/esp32_sketch/esp32_sketch.ino
+++ task2/esp32_sketch/esp32_sketch.ino
@@ -1,11 +1,15 @@
-int VRX_PIN = 22;
-int VRY_PIN = 23;
+int VRX_PIN = 4;
+int VRY_PIN = 0;
+int OUT_PIN = 16;
+int JBUTTON = 5;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(VRX_PIN, INPUT);
   pinMode(VRY_PIN, INPUT);
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(JBUTTON, INPUT);
 }
 
 void loop() {
@@ -16,4 +20,5 @@ void loop() {
   Serial.println(vrxVal);
   Serial.print("VRY: ");
   Serial.println(vryVal);
+  // Serial.println("Button: " + digitalRead(JBUTTON));
 }
