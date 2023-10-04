827d87dc3c8bf422c23d026373003c4e3a687d81
Darwin Do <darwin78913@gmail.com>
2023-10-04 17:34:40 -0400

    add sketch

--- task2/esp32_sketch/esp32_sketch.ino
+++ task2/esp32_sketch/esp32_sketch.ino
@@ -1,15 +1,13 @@
 int VRX_PIN = 34;
 int VRY_PIN = 35;
-int OUT_PIN = 16;
-int JBUTTON = 5;
+int SWITCH_PIN = 25;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(VRX_PIN, INPUT);
   pinMode(VRY_PIN, INPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(JBUTTON, INPUT);
+  pinMode(SWITCH_PIN, INPUT);
 }
 
 void loop() {
@@ -18,7 +16,16 @@ void loop() {
   printOut += analogRead(VRX_PIN);
   printOut += " VRY: ";
   printOut += analogRead(VRY_PIN);
+  printOut += " SWITCH: ";
+  printOut += digitalRead(SWITCH_PIN);
   // Serial.println("VRX: " + analogRead(VRX_PIN) + " VRY: " + analogRead(VRY_PIN));
   // Serial.println(printOut);
-  Serial.println("Button: " + digitalRead(JBUTTON));
+  Serial.println(String("VRX: ") + analogRead(VRX_PIN));
+  Serial.println(String("VRY: ") + analogRead(VRY_PIN));
+  Serial.println(String("SWITCH: ") + digitalRead(SWITCH_PIN));
+
+  // THIS PRINTS OUT WEIRD STUFF?!?!
+  // Serial.println("VRX: " + analogRead(VRX_PIN));
+  // Serial.println("VRY: " + analogRead(VRY_PIN));
+  // Serial.println("SWITCH: " + digitalRead(SWITCH_PIN));
 }
