204cefbcbe9721b33d2c88f56d7e12ceefd2c32f
Darwin Do <darwin78913@gmail.com>
2023-10-06 20:21:56 -0400

    sketch changes

--- task2/rainplayer_esp32/rainplayer_esp32.ino
+++ task2/rainplayer_esp32/rainplayer_esp32.ino
@@ -1,23 +1,21 @@
 int VRX_PIN = 34;
-int VRY_PIN = 35;
-int SWITCH_PIN = 25;
+int SWITCH_PIN = 26;
 int BUTTON_PIN = 19;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(VRX_PIN, INPUT);
-  pinMode(VRY_PIN, INPUT);
   pinMode(SWITCH_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Serial.println(String("VRX: ") + analogRead(VRX_PIN));
+  Serial.println(String("VRX: ") + analogRead(VRX_PIN));
   // Serial.println(String("VRY: ") + analogRead(VRY_PIN));
-  // Serial.println(String("SWITCH: ") + !digitalRead(SWITCH_PIN));
-  // Serial.println(String("BUT: ") + digitalRead(BUTTON_PIN));
+  Serial.println(String("SWITCH: ") + !digitalRead(SWITCH_PIN));
+  Serial.println(String("BUT: ") + !digitalRead(BUTTON_PIN));
 
 
   // THIS PRINTS OUT WEIRD STUFF?!?!
